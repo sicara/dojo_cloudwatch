79517fc91b158d520ead3bfaa1e3dfe775e1fb31
Florian CARRA <florianc@sicara.com>
2020-03-13 19:48:11 +0100

    Update lambda handlers and serverless script

--- demo/lambda/consumer.py
+++ demo/lambda/consumer.py
@@ -1,8 +1,35 @@
+import os
 import time
+import uuid
+from datetime import date
+from decimal import Decimal
+
+import boto3
+
+REGION_NAME = "eu-west-1"
+DDB_NAME = os.getenv('DDB_NAME')
+
+
+def write_to_dynamodb(table_name: str, item: dict):
+    dynamodb = boto3.resource("dynamodb", region_name=REGION_NAME)
+    table = dynamodb.Table(table_name)
+    table.put_item(Item=item)
 
 
 def handler(event, context):
     sqs_message = event["Records"][0]["body"]
-    if int(sqs_message) % 2 == 0:
-        time.sleep(15)
-    print(sqs_message)
+    print(f"Message: {sqs_message}")
+    ddb_item = {
+        "_id": str(uuid.uuid4()),
+        "_created": Decimal(time.time()),
+        "message": sqs_message
+    }
+
+    # PROCESS 1 - INFRA ERROR
+    time.sleep(2 * int(sqs_message))
+
+    # PROCESS 2 - APPLI ERROR
+    if sqs_message == "3":
+        ddb_item["date"] = date.today()
+
+    write_to_dynamodb(table_name=DDB_NAME, item=ddb_item)
--- demo/lambda/producer.py
+++ demo/lambda/producer.py
@@ -1,16 +1,19 @@
 import os
+import time
 from typing import List
 
 import boto3
 
+REGION_NAME = "eu-west-1"
 SQS_QUEUE_NAME = os.getenv('SQS_QUEUE_NAME')
 
 
 def send_messages_to_queue(messages: List[str], sqs_queue_name: str):
-    sqs = boto3.resource("sqs")
+    sqs = boto3.resource("sqs", region_name=REGION_NAME)
     queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
     for message in messages:
         queue.send_message(MessageBody=message)
+        time.sleep(0.5)
 
 
 def handler(event, context):
